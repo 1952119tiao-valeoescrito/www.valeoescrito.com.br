e54b3d9e223f06116c341ceaa7dcbd272f5a2b8a
SEBASTIAO FRANCISCO CHAGAS FILHO <franciscochagassebastiaofilho@gmail.com>
2025-03-08 16:21:02 -0300

    Create App.jsx
    Ratificando.

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ApostaForm from "./components/ApostaForm";
+
+function App() {
+  return (
+    <div>
+      <h1>Loteria de Prognósticos</h1>
+      <ApostaForm />
+    </div>
+  );
+}
+
+export default App;
