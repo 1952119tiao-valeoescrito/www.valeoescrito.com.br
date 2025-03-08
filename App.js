ef84fb5054cd74ea09ae10e12647be2f681e7008
SEBASTIAO FRANCISCO CHAGAS FILHO <franciscochagassebastiaofilho@gmail.com>
2025-03-08 16:05:12 -0300

    Update App.js
    Ratificando.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import LoteriaPrognosticosABI from "./contracts/LoteriaPrognosticos.json";
 
-const contractAddress = "0x..."; // Endereço do contrato deployado
+const contractAddress = "0x8fa024467a4ab1286e78da4a876d18abaea667a38f1e2679069ce6b1ee8663d7"; // Endereço do contrato deployado
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 const contract = new ethers.Contract(contractAddress, LoteriaPrognosticosABI, signer);
