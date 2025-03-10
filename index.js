7222a6e55eb340a9a2f7a140c757b61e5bf82eb2
SEBASTIAO FRANCISCO CHAGAS FILHO <franciscochagassebastiaofilho@gmail.com>
2025-03-10 13:26:23 -0300

    Update index.js
    Errei.

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 // index.js
 const ethers = require('ethers');
 
-// Substitua pela sua chave API do Infura ou Alchemy
-const provider = new ethers.JsonRpcProvider("SUA_CHAVE_API_AQUI");
-
-async function main() {
-  try {
-    const blockNumber = await provider.getBlockNumber();
-    console.log("Número do último bloco:", blockNumber);
-  } catch (error) {
-    console.error("Erro ao obter o número do bloco:", error);
-  }
-}
-
-main();
+console.log('Olá, mundo!');
+console.log('Versão do Ethers:', ethers.version);
